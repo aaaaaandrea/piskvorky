939daa1309dd1c5979e70bfc5f396036bebcc7eb
Andrea Židkova <zidkovaandrea66@gmail.com>
2021-03-22 17:52:00 +0100

    prepracovani

--- publick/app.js
+++ publick/app.js
@@ -1,51 +1,50 @@
-const infoDisplay = document.getElementById('info')
-let currentPlayer = 'player0'
-let playerNum = 0
-let p0Ready = false
-let p1Ready = false
-
-
-
 const socket = io()
 
-//player num
-socket.on('player-number', num => {
-    if (num === -1) {
-        infoDisplay.innerHTML = "<h1>Server full</h1>"
-    } else {
-        playerNum = parseInt(num)
-        if (playerNum === 1) currentPlayer = "player1"
-        console.log(playerNum)
-    }
+socket.on('message', message => {
+    console.log(message)
 })
-
-//connected disconnected
-
-socket.on('player-connection', num => {
-    console.log(`Player number ${num} has connected or disconnected`)
-})
-
 
 //GAME
+
+
 
 let tableRow = document.getElementsByTagName('tr')
 let tableCell = document.getElementsByTagName('td')
 let tableSlot = document.querySelector('.slot')
+let color = ['red','blue']
+let clicks = 0
 
-for (let i = 0; i<tableCell.length; i++){
+/*for (let i = 0; i<tableCell.length; i++){
     tableCell[i].addEventListener('click', (e) =>{
        console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`)
        
     })
+}*/
+
+for (let i = 0; i < tableCell.length; i++) {
+    
+    tableCell[i].addEventListener('click', (e) => {
+        clicks ++
+        e.preventDefault()
+        tableCell[i].style.backgroundColor = color[clicks%2]
+        let position = [e.target.parentElement.rowIndex, e.target.cellIndex]
+        socket.emit('position', position)
+    })
+    tableCell[i].style.backgroundColor = 'white'
 }
 
+
+/*
 Array.prototype.forEach.call(tableCell, (cell) => {
+    box.push(cell)
+
     cell.addEventListener('click', (e)=>{
+        e.preventDefault()
         cell.style.backgroundColor = 'red'
+        let position = [e.target.parentElement.rowIndex, e.target.cellIndex ]
+        box.push(position)
+        socket.emit('position',position)
     })
     cell.style.backgroundColor = 'white'
-})
+})*/
 
-/*function changeColor (e){
-    style.backgroundColor = 'red'
-}*/
--- server.js
+++ server.js
@@ -1,46 +1,36 @@
-const express = require('express')
 const path = require('path')
 const http = require('http')
-const PORT = process.env.PORT || 3030
+const express = require('express')
 const socketio = require('socket.io')
+
+
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-//static folder
-app.use(express.static(path.join(__dirname, "publick")))
+//set static folder
+app.use(express.static(path.join(__dirname, 'publick')))
 
-//start sever
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-
-//socket request from web client
-
-const connections = [null, null]
-
-
+//run when client connects
 io.on('connection', socket => {
-    //console.log('Nove pripojeni')
+    // console.log('New ws conection')
 
-    //najdi dostupne cislo hrace
-    let playerIndex = -1
-    for (const i in connections) {
-        if (connections[i] === null) {
-            playerIndex = i
-            break
-        }
-    }
+    socket.emit('message', 'welcome')
 
+    //broadcast uzivatel pripojeni
+    socket.broadcast.emit('message', 'New player joined')
 
-    //tell the number of player
-    socket.emit('player-number', playerIndex)
+    //broadcast pri odpojeni
+    socket.on('disconnect', () => {
+        io.emit('message', 'Player has left the game')
+    })
 
-    console.log(`Player ${playerIndex} has connected`)
-
-    //ignoruj tretiho
-    if (playerIndex === -1) return
-
-    connections[playerIndex] = false
-
-    // sdelit pripojeni ostatim
-    socket.broadcast.emit('player-connection', playerIndex)
+    //nasloucha zpravu o pozici
+    socket.on('position', (position) => {
+        io.emit('message', position)
+    })
 })
+
+const PORT = 3030 || process.env.PORT
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
