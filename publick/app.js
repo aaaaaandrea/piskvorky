1a5c9aa70a926962de4202bb358b14063efa56b8
Andrea Židkova <zidkovaandrea66@gmail.com>
2021-03-23 20:00:34 +0100

    tlacitko start

--- publick/app.js
+++ publick/app.js
@@ -1,14 +1,6 @@
-const socket = io()
-
-socket.on('message', message => {
-    console.log(message)
-})
-
-
-
 //GAME
 
-
+let button = document.getElementById('button')
 
 let tableRow = document.getElementsByTagName('tr')
 let tableCell = document.getElementsByTagName('td')
@@ -17,29 +9,38 @@ let color = ['red', 'blue']
 let clicks = 0
 let blockColor, position
 
+button.addEventListener('click', () => {
+    const socket = io()
 
-for (let i = 0; i < tableCell.length; i++) {
-
-    tableCell[i].addEventListener('click', (e) => {
-        e.preventDefault()
-        // kontrola jestli pole neni obsazeno
-        if (tableCell[i].style.backgroundColor == 'white') {
-            socket.emit('clicks')
-            blockColor = [i, color[clicks % 2]]
-            socket.emit('blockColor', blockColor)
-        }
-        // zjisteni pozice na tabulce, jen pro kontrolu
-        position = [e.target.parentElement.rowIndex, e.target.cellIndex]
-        // odeslani pozice na server
-        socket.emit('position', position)
+    //prijme zpravy ze serveru a vypise do console
+    socket.on('message', message => {
+        console.log(message)
     })
-    tableCell[i].style.backgroundColor = 'white'
-}
 
-socket.on('clicksPlus', function plusClick () {
-    clicks++
-})
+    for (let i = 0; i < tableCell.length; i++) {
 
-socket.on('changeColor', changedBlock => {
-    tableCell[changedBlock[0]].style.backgroundColor = changedBlock[1]
+        tableCell[i].addEventListener('click', (e) => {
+            e.preventDefault()
+            // kontrola jestli pole neni obsazeno
+            if (tableCell[i].style.backgroundColor == 'white') {
+                socket.emit('clicks')
+                blockColor = [i, color[clicks % 2]]
+                socket.emit('blockColor', blockColor)
+            }
+            // zjisteni pozice na tabulce, jen pro kontrolu
+            position = [e.target.parentElement.rowIndex, e.target.cellIndex]
+            // odeslani pozice na server
+            socket.emit('position', position)
+        })
+        tableCell[i].style.backgroundColor = 'white'
+    }
+
+
+    socket.on('clicksPlus', function plusClick() {
+        clicks++
+    })
+
+    socket.on('changeColor', changedBlock => {
+        tableCell[changedBlock[0]].style.backgroundColor = changedBlock[1]
+    })
 })
